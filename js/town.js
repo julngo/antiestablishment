bba4b7f0a64eb4c1a28db6e9d9dfd5b7467e26ae
Su <rice74@uw.edu>
2018-04-24 23:22:26 -0700

    updated town content, responsive character files

--- js/town.js
+++ js/town.js
@@ -1,10 +1,16 @@
+
 $( document ).ready(function() {
+  $('body').css('overflow','hidden');
   setTimeout(function(){
     $('#loader').css('opacity', '0');
     $('#loader').css('z-index', '0');
     $('body').css('overflow','auto');
     $('.news-content:not(:first)').hide();
-  }, 1500);
+  }, 250);
+  $('.e-left').each(function(key, val){
+    var path = "url(../img/town/event"+(key+1)+".jpg)";
+    $(this).css('background-image', path);
+  })
 });
 
 $('.news-title h2').click(function(){
